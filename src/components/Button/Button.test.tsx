6c064dfc9a8a4d791068e4645a76d61dc0b8af14
Marissa Steindel <marissa.steindel@gmail.com>
2023-01-30 15:14:36 -0600

    add comments to button.test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,8 +1,12 @@
+// import React from the node_modules
 import React from "react";
+// import the render method from the React Testing Library
 import { render } from "@testing-library/react";
-
+// import the Button.tsx component from the current directory
 import Button from "./Button";
 
+// the test
+// to know more about tests, read the React Testing Library documentation
 describe("Button", () => {
   test("renders the Button component", () => {
     render(<Button label="Hello world!" />);
