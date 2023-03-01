b59d1fa9a5a347411f3ecf0a9994c5e7afc14f8c
Marissa Steindel <marissa.steindel@gmail.com>
2023-03-01 16:17:55 -0600

    add styling and story arg types to Text component

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Text from "./Text";
 
+// export interface TextProps {
+//   content: string;
+// }
+
+// details for the Storybook sidebar
 export default {
   title: "Marissa/Text",
   component: Text,
+  argTypes: {
+    backgroundColor: { control: 'color' }
+   },
 } as ComponentMeta<typeof Text>;
 
+// build a template for the story
 const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
 
+// bind values to the story template
 export const Visibility = Template.bind({});
 Visibility.args = {
   content: "This is a line of text!",
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-// import './Text.css';
+import styled from 'styled-components';
 
+// define the props of the component
 export interface TextProps {
   content: string;
 }
 
+// define the styles of the HTML tags of the component
+const StyledText = styled.p`
+  background-color: aliceblue;
+`;
+
+// define the component
+// pass it the React props constrained by the interface
+// return the component with the styledcomponents wrapper
 const Text = (props: TextProps) => {
-  return <p>{props.content}</p>;
+  return(
+    <StyledText>
+      {props.content}
+    </StyledText>
+  );
 };
 
 export default Text;
