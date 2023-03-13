37b06416aa137e2fbc79e2bfe0a539e62fa33d7d
Marissa Steindel <marissa.steindel@gmail.com>
2023-03-13 15:16:32 -0500

    add hover style to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -11,6 +11,9 @@ const StyledButton = styled.button<ButtonProps>`
     padding: 10px 15px;
     /* font-family: Arial, Helvetica, sans-serif; */
     cursor: ${props => props.disabled ? "not-allowed" : "default"};
+    &:hover{
+      background-color: pink;
+    };
 `;
 
 // instantiate the Button object that takes an argument of type ButtonProps
